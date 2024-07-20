8959971aa918f203dd6061a7f34323fbbaf43772
MrPowerUp82 <gustavohenrique8282@hotmail.com>
2024-07-20 17:03:10 -0300

    `chore: Update FilamentSqlField to use local CSS and JS files`

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,38 +1,33 @@
 <x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
-    <div x-data="{ state: $wire.entangle('{{ $getStatePath() }}'), tables: {{ $getDatabaseTables() }}, isDark: {{ $getDark() }} }" style="width: 100%; font-size: 0.875rem; line-height: 1.25rem;" x-init="() => {
-        $nextTick(() => {
-            const options = {
-                mode: 'text/x-mysql',
-                indentWithTabs: true,
-                smartIndent: true,
-                lineNumbers: true,
-                matchBrackets: true,
-                autofocus: true,
-                theme: isDark ? 'dracula' : 'default',
-                extraKeys: {
-                    'Ctrl-Space': 'autocomplete'
-                },
-                hintOptions: {
-                    tables: tables
-                }
-            };
-            window.editor = CodeMirror.fromTextArea($refs.editor, options);
-            window.editor.on('change', (cMirror) => {
-                state = cMirror.getValue();
-                $refs.editor.value = cMirror.getValue();
+    <div x-data="{ state: $wire.entangle('{{ $getStatePath() }}'), tables: {{ $getDatabaseTables() }}, isDark: {{ $getDark() }} }" x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('codemirror5-css-cdn', package: 'mrpowerup/filament-sql-field'))]" x-load-js="[@js(\Filament\Support\Facades\FilamentAsset::getScriptSrc('codemirror5-js-cdn', package: 'mrpowerup/filament-sql-field'))]"
+        x-load-js="[@js(\Filament\Support\Facades\FilamentAsset::getScriptSrc('matchbrackets-js-cdn', package: 'mrpowerup/filament-sql-field'))]" x-load-js="[@js(\Filament\Support\Facades\FilamentAsset::getScriptSrc('sql-js-cdn', package: 'mrpowerup/filament-sql-field'))]" x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('showhint-css-cdn', package: 'mrpowerup/filament-sql-field'))]"
+        x-load-js="[@js(\Filament\Support\Facades\FilamentAsset::getScriptSrc('showhint-js-cdn', package: 'mrpowerup/filament-sql-field'))]" x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('dracula-theme', package: 'mrpowerup/filament-sql-field'))]" x-load-js="[@js(\Filament\Support\Facades\FilamentAsset::getScriptSrc('sqlhint-js-cdn', package: 'mrpowerup/filament-sql-field'))]">
+        <div style="width: 100%; font-size: 0.875rem; line-height: 1.25rem;" x-init="() => {
+            $nextTick(() => {
+                const options = {
+                    mode: 'text/x-mysql',
+                    indentWithTabs: true,
+                    smartIndent: true,
+                    lineNumbers: true,
+                    matchBrackets: true,
+                    autofocus: true,
+                    theme: isDark ? 'dracula' : 'default',
+                    extraKeys: {
+                        'Ctrl-Space': 'autocomplete'
+                    },
+                    hintOptions: {
+                        tables: tables
+                    }
+                };
+                window.editor = CodeMirror.fromTextArea($refs.editor, options);
+                window.editor.on('change', (cMirror) => {
+                    state = cMirror.getValue();
+                    $refs.editor.value = cMirror.getValue();
+                });
             });
-        });
-    }"
-    x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('codemirror5-css-cdn', package: 'mrpowerup/filament-sql-field'))]"
-    x-load-js="[@js(\Filament\Support\Facades\FilamentAsset::getScriptSrc('codemirror5-js-cdn', package: 'mrpowerup/filament-sql-field'))]"
-    x-load-js="[@js(\Filament\Support\Facades\FilamentAsset::getScriptSrc('matchbrackets-js-cdn', package: 'mrpowerup/filament-sql-field'))]"
-    x-load-js="[@js(\Filament\Support\Facades\FilamentAsset::getScriptSrc('sql-js-cdn', package: 'mrpowerup/filament-sql-field'))]"
-    x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('showhint-css-cdn', package: 'mrpowerup/filament-sql-field'))]"
-    x-load-js="[@js(\Filament\Support\Facades\FilamentAsset::getScriptSrc('showhint-js-cdn', package: 'mrpowerup/filament-sql-field'))]"
-    x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('dracula-theme', package: 'mrpowerup/filament-sql-field'))]"
-    x-load-js="[@js(\Filament\Support\Facades\FilamentAsset::getScriptSrc('sqlhint-js-cdn', package: 'mrpowerup/filament-sql-field'))]"    
-    x-cloak
-        wire:ignore>
-        <textarea x-ref="editor" x-bind:value="state"></textarea>
+        }" x-cloak wire:ignore>
+            <textarea x-ref="editor" x-bind:value="state"></textarea>
+        </div>
     </div>
+
 </x-dynamic-component>
